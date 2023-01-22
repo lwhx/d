58fc7af343b2964c58b74c1cea40d86fd49f98c9
blackice <45483405+lwhx@users.noreply.github.com>
2023-01-22 19:00:30 +0800

    Update dk.sh

--- dk.sh
+++ dk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+firewall-cmd --zone=public --add-port=9527/tcp--permanent
+firewall-cmd --zone=public --add-port=5443/tcp --permanent
+firewall-cmd --zone=public --add-port=54321/tcp --permanent
+firewall-cmd --zone=public --add-port=81tcp --permanent
+firewall-cmd --zone=public --add-port=8081/tcp --permanent
+firewall-cmd --zone=public --add-port=6005/tcp --permanent
+firewall-cmd --zone=public --add-port=29432/tcp --permanent
 firewall-cmd --zone=public --add-port=6000/tcp --permanent
 firewall-cmd --zone=public --add-port=6001/tcp --permanent
 systemctl restart firewalld.service
